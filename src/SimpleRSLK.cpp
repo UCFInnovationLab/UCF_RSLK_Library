7d75a53193aad3eefc67cc69a4f166bf1d92e909
crabmeat99 <harper@cs.ucf.edu>
2023-10-15 19:59:01 -0400

    Update to fix line following

--- src/SimpleRSLK.cpp
+++ src/SimpleRSLK.cpp
@@ -5,7 +5,7 @@ Romi_Motor_Power	motor[2];
 Bump_Switch			bump_sw[6];
 uint16_t calMin[LS_NUM_SENSORS], calMax[LS_NUM_SENSORS];
 
-QTRSensors qtr;
+QTRSensorsUCF qtr;
 
 void setupRSLK() {
 	motor[0].begin(MOTOR_L_SLP_PIN,MOTOR_L_DIR_PIN,MOTOR_L_PWM_PIN);
@@ -24,16 +24,19 @@ void setupRSLK() {
 	dst_sensor[1].begin(SHRP_DIST_C_PIN,INPUT_PULLDOWN);
 	dst_sensor[2].begin(SHRP_DIST_R_PIN,INPUT_PULLDOWN);
 
-	qtr.setTypeRC();
-	qtr.setSensorPins((const uint8_t[]){QTR_7, QTR_6, QTR_5, QTR_4, QTR_3, QTR_2, QTR_1, QTR_0}, LS_NUM_SENSORS);
-	qtr.setEmitterPins(QTR_EMITTER_PIN_ODD, QTR_EMITTER_PIN_EVEN);
+	// qtr.setTypeRC();
+	// qtr.setSensorPins((const uint8_t[]){QTR_7, QTR_6, QTR_5, QTR_4, QTR_3, QTR_2, QTR_1, QTR_0}, LS_NUM_SENSORS);
+	// qtr.setEmitterPins(QTR_EMITTER_PIN_ODD, QTR_EMITTER_PIN_EVEN);
+	// disableMotor(BOTH_MOTORS);
+	qtr.setSensorPin(QTR_0);
 	disableMotor(BOTH_MOTORS);
 
-	for (uint8_t x = 0; x < LS_NUM_SENSORS; x++)
-	{
-		calMin[x] = 5000;
-		calMax[x] = 0;
-	}
+
+	// for (uint8_t x = 0; x < LS_NUM_SENSORS; x++)
+	// {
+	// 	calMin[x] = 5000;
+	// 	calMax[x] = 0;
+	// }
 
 }
 
@@ -147,74 +150,8 @@ void setRawMotorSpeed(uint8_t motorNum, uint8_t speed) {
 	}
 }
 
-void readLineSensor(uint16_t* sensorValues) {
-	qtr.read(sensorValues,QTRReadMode::OddEven);
-}
-
-void clearMinMax(uint16_t *sensorMin,uint16_t *sensorMax) {
-	for(int x = 0;x<LS_NUM_SENSORS;x++)
-	{
-		sensorMin[x] = 9000;
-		sensorMax[x] = 0;
-	}
-}
-
-
-void readCalLineSensor(uint16_t* sensorValues,
-					   uint16_t* calVal,
-					   uint16_t *sensorMinVal,
-					   uint16_t *sensorMaxVal,
-					   uint8_t mode)
-{
-	for(int x = 0;x<LS_NUM_SENSORS;x++)
-	{
-		if(mode)
-		{
-			calVal[x] = 0;
-			if(sensorValues[x] < sensorMinVal[x])
-				calVal[x] = map(sensorValues[x],0,sensorMinVal[x],1000,0);
-		} else {
-			calVal[x] = 0;
-			if(sensorValues[x] > sensorMaxVal[x])
-				calVal[x] = map(sensorValues[x],sensorMaxVal[x],2500,0,1000);
-		}
-	}
-}
-
-uint32_t getLinePosition(uint16_t* calVal, uint8_t mode)
-{
-	uint32_t avg = 0; // this is for the weighted total
-	uint32_t sum = 0; // this is for the denominator, which is <= 64000
-
-	uint32_t _lastPosition;
-	for (uint8_t i = 0; i < LS_NUM_SENSORS; i++)
-	{
-		uint16_t value = calVal[i];
-
-		// only average in values that are above a noise threshold
-		if (value > 50)
-		{
-			avg += (uint32_t)value * (i * 1000);
-			sum += value;
-		}
-	}
-
-	_lastPosition = avg / sum;
-	return _lastPosition;
-}
-
-void setSensorMinMax(uint16_t *sensor,uint16_t *sensorMin,uint16_t *sensorMax) {
-	for(int x = 0;x<LS_NUM_SENSORS;x++)
-	{
-		if(sensor[x] < sensorMin[x])
-		{
-			sensorMin[x] = sensor[x];
-		}
-		if(sensor[x] > sensorMax[x])
-		{
-				sensorMax[x] = sensor[x];
-		}
-	}
+uint16_t readLineSensor() {
+	return qtr.read();
 }
 
 void setupWaitBtn(uint8_t btn) {
